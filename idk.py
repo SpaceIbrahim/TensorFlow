0de3898e29d18dd27f0cf778b53b197feef5c945
SpaceIbrahim <ibrahimm4566@gmail.com>
2023-10-07 18:05:40 -0400

    Imported Dataset

---
+++ idk.py
@@ -0,0 +1,12 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+from tensorflow import feature_column as fc
+import tensorflow as tf
+
+# Load dataset.
+dftrain = pd.read_csv('Dataset/train.csv') # training data
+dfeval = pd.read_csv('Dataset/eval.csv') # testing data
+y_train = dftrain.pop('survived')
+y_eval = dfeval.pop('survived')
